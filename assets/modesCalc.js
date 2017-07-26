6f00c284bd7d996a981e13a7f043dbca0d0abf6b
Emma Cliffe <cspehj@mapc-lapsup18.campus.bath.ac.uk>
2017-07-26 14:22:41 +0100

    removed textsize change and workshop references

--- assets/modesCalc.js
+++ assets/modesCalc.js
@@ -1,19 +1,92 @@
 function printvis(num) {
-    return "hallo " + num;
+    var vis = "";
+
+    vis += '<div id="vis_desc">';
+    vis += '<div class="logofloat"><img src="images/vis_sing.gif" alt="" width="100" height="100" /></div>';
+    vis += '<h3>Visual Learners</h3>'
+    vis += '<p><strong>have a preference for learning from graphs and diagrams representing information</strong></p>';
+    vis += "<p>Your score for the visual learning style: " + num + "</p>";
+    vis += '<ul>';
+    vis += '<lh>Visual learners...</lh>';
+    vis += '<li>like information presented in graphs, charts,  diagrams etc.</li>';
+    vis += '<li>will be as comfortable reading and using symbols  as the text they replace</li>';
+    vis += '</ul>';
+    vis += '<ul>';
+    vis += '<lh>If this appeals to you, you could...</lh>';
+    vis += '<li>try using diagrams, graphs or charts to help  understand new topics. Look for ways to alter the diagrams to show different  examples, special cases, etc. </li>';
+    vis += '<li>use different colours to make your notes easier  to read.<br /> e.g. underline definitions in one colour, examples in a second colour and  theorems in a third      </li>';
+    vis += '</ul>';
+    vis += '</div>';
+    return vis;
 }
 
 function printaud(num) {
-    return "aud " + num;
+    var aud = "";
+    aud += '<div id="aud_desc">';
+    aud += '<div class="logofloat"><img src="images/aud_sing.gif" alt="" width="100" height="100" /></div>';
+    aud += '<h3>Auditory Learners</h3>'
+    aud += '<p><strong>have a preference for learning by listening and discussion</strong></p>';
+    aud += "<p>Your score for the auditory learning style: " + num + "</p>";
+    aud += '<ul>';
+    aud += '<lh>Auditory learners...</lh>';
+    aud += '<li>like lecturers to speak about the topic in  detail</li>';
+    aud += '<li>like learning through discussion with tutors and  fellow students</li>';
+    aud += '</ul>';
+    aud += '<ul>';
+    aud += '<lh>If this appeals to you, you could...</lh>';
+    aud += '<li>try discussing new topics with a group of fellow  students, or take turns to explain the concepts to each other.</li>';
+    aud += '<li>talk through examples to better understand  what&#39;s happening.</li>';
+    aud += '</ul>';
+    aud += '</div>';
+
+    return aud;
 }
 
 
 function printraw(num) {
-    return "raw " + num;
+    var raw = "";
+    raw += '<div id="raw_desc">';
+    raw += '<div class="logofloat"><img src="images/raw_sing.gif" alt="" width="100" height="100" /></div>';
+    raw += '<h3>Reading/Writing Learners</h3>';
+    raw += '<p><strong>have a preference for learning from information presented as text</strong></p>';
+    raw += "<p>Your score for the reading/writing learning style: " + num + "</p>";
+    raw += '<ul>';
+    raw += '<lh>Reading/Writing learners...</lh>';
+    raw += '<li>like information presented as words - this may  be blocks of text or lists of bullet points</li>
+<li>will read mathematical symbols and operators as if they are text</li>';
+    raw += '</ul>';
+    raw += '<ul>';
+    raw += '<lh>If this appeals to you, you could...</lh>';
+    raw += '<li>make a short summary of a new concept and read  it back. Using different words and phrases where possible will help you ensure  you don&#39;t just copy parrot-fashion.</li>';
+    raw += '<li>when faced with a diagram, label it using words  and write a brief description of what it shows.</li>';
+    raw += '</ul>';
+    raw += '</div>';
+
+    return raw;
 }
 
 
 function printkin(num) {
-    return "kins " + num;
+    var kin = "";
+    kin += '<div id="kin_desc">';
+    kin += '<div class="logofloat"><img src="images/kin_sing.gif" alt="" width="100" height="100" /></div>';
+    kin += '<h3>Kinaesthetic Learners</h3>';
+    kin += '<p><strong>have a preference for learning from practice and movement</strong></p>';
+    kin += "<p>Your score for the kinaesthetic learning style: " + num + "</p>";
+    kin += '<ul>';
+    kin += '<lh>Kinaesthetic learners...</lh>';
+    kin += '<li>like learning by using practical examples</li>';
+    kin += '<li>like using interactivity and/or physical props as appropriate</li>';
+    kin += '</ul>';
+    kin += '<ul>';
+    kin += '<lh>If this appeals to you, you could...</lh>';
+    kin += '<li>when starting a new topic, look for applications  and examples, preferably in the real world, and try to understand how they fit  with the theory</li>';
+    kin += '<li>when trying to learn a process, write each step  on a separate card and practice putting them in order until it becomes easy</li>';
+    kin += '<li>keep a supply of modelling materials such as plasticine or drinking straws. You may find you are able to make a model instead of just visualising something.</li>';
+    kin += '</ul>';
+    kin += '</div>';
+
+    return kin;
 }
 
 function modesCalc() {
@@ -90,8 +163,10 @@ function modesCalc() {
 
 
     for (var i = 0; i < reses.length; i++) {
-	all += reses[i].fn(reses[i].val) + "<p>";
+	all += reses[i].fn(reses[i].val);
     }
+
+    all += '<p>For suggestions on how to learn, see <a href="some-ideas.shtml">Some Ideas</a><br />For more information, see <a href="further-reading.shtml">Further Reading</a></p>';
 
     everything.innerHTML = all;
     return false;
